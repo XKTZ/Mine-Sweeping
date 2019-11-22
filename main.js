a13c1522412c53523943d43d6d40293ce6c54f07
YiDi Chen <cheny5351@statech-103-13.classroom.ycdsb.ca>
2019-11-22 13:12:15 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function () {
+    const backArr = []
+    // backboard -> the board of mines
+    let backBoard = [];
+    // frontboard -> the board show to users
+    let frontBoard = [];
+    let win = false;
+    function initBoard() {
+        for(let i = 0; i < 11; i ++) {
+            
+        }
+    }
+});
